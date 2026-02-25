0bc4501687bcd1784286d6ec9ffc0b0fa887fd7c
pangminwhere <qoendqh@naver.com>
2026-02-25 22:57:45 +0900

    feat : Initial settings

---
+++ src/main/java/com/sh/Ram/ResaleAuctionMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.sh.Ram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaleAuctionMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResaleAuctionMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sh/Ram/ResaleAuctionMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.Ram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResaleAuctionMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
