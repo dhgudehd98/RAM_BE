a2667711d37876ee8bb9bf410be58874560e6854
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-03-06 17:14:04 +0900

    20260304_feat : 전체 상품 조회 기능 구현

---
+++ src/main/java/com/sh/Ram/product/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.sh.Ram.product.service;
+
+import com.sh.Ram.product.dto.ProductDto;
+import com.sh.Ram.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Page<ProductDto> findAllProduct(String sort, int page) {
+
+        Pageable pageable;
+
+        // 정렬 조건에 따라서 Paging 분류 방법 다르게
+        if (sort != null) pageable = PageRequest.of(page, 10, Sort.by(sort).descending());
+        else {
+            pageable = PageRequest.of(page, 10, Sort.by("name").descending());
+        }
+
+        return productRepository.findAll(pageable)
+                .map(ProductDto::from);
+
+    }
+}
