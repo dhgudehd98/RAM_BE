f8ef3a9ae5169728cdc18ead7374812d13f53fbb
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-03-08 02:04:51 +0900

    20260308_feat : Customizing Exception Handler

---
+++ src/main/java/com/sh/Ram/common/exception/member/MemberException.java
@@ -0,0 +1,8 @@
+package com.sh.Ram.common.exception.member;
+
+public class MemberException extends RuntimeException{
+
+    public MemberException(String message) {
+        super(message);
+    }
+}
