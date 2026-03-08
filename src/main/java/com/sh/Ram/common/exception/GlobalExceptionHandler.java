ea9aa4eee5098d2966df752d446ba56c0bf3404c
pangminwhere <qoendqh@naver.com>
2026-03-08 23:50:27 +0900

    20260308_feat: Auction 예외 처리 및 GlobalExceptionHandler 추가

--- src/main/java/com/sh/Ram/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sh/Ram/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sh.Ram.common.exception;
 
+import com.sh.Ram.common.exception.auction.AuctionException;
 import com.sh.Ram.common.exception.member.MemberException;
 import com.sh.Ram.common.exception.product.ProductException;
 import jakarta.security.auth.message.AuthException;
@@ -22,7 +23,15 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MemberException.class)
-    public ResponseEntity<Map<String, String>> handleMember(ProductException e) {
+    public ResponseEntity<Map<String, String>> handleMember(MemberException e) {
+        Map<String, String> res = new HashMap<>();
+        res.put("result", "N");
+        res.put("message", e.getMessage());
+        return ResponseEntity.badRequest().body(res);
+    }
+
+    @ExceptionHandler(AuctionException.class)
+    public ResponseEntity<Map<String, String>> handleAuction(AuctionException e) {
         Map<String, String> res = new HashMap<>();
         res.put("result", "N");
         res.put("message", e.getMessage());
---
+++ src/main/java/com/sh/Ram/common/exception/auction/AuctionException.java
@@ -0,0 +1,8 @@
+package com.sh.Ram.common.exception.auction;
+
+public class AuctionException extends RuntimeException {
+
+    public AuctionException(String msg) {
+        super(msg);
+    }
+}
