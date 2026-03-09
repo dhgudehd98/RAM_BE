0590846a7a76edba56b67dc02d4ddd383dea080c
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-03-10 02:03:18 +0900

    20260308_feat : 상품 이미지 LLM 연동

---
+++ src/main/java/com/sh/Ram/product/dto/LlmResponseDto.java
@@ -0,0 +1,14 @@
+package com.sh.Ram.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LlmResponseDto {
+    private String response;
+}
