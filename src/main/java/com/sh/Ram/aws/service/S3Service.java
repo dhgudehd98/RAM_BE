7bddcaeebdf60c2e1a7c111835ba7579f56ea520
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-03-13 12:38:07 +0900

    20260313_Init : AWS S3 기본 설정

---
+++ src/main/java/com/sh/Ram/aws/service/S3Service.java
@@ -0,0 +1,16 @@
+package com.sh.Ram.aws.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+}
