68f91c34c73b5fc02c6746cb85998a46110a5898
pangminwhere <qoendqh@naver.com>
2026-03-20 01:31:07 +0900

    20260320_refactor: BidException 가격 mismatch 예외 메시지 포맷 변경

--- src/main/java/com/sh/Ram/common/exception/bid/BidException.java
+++ src/main/java/com/sh/Ram/common/exception/bid/BidException.java
@@ -1,8 +1,22 @@
 package com.sh.Ram.common.exception.bid;
 
+import lombok.Getter;
+
+@Getter
 public class BidException extends RuntimeException {
+
+    private final Integer currentPrice;
+    private final Integer nextBidPrice;
 
     public BidException(String msg) {
         super(msg);
+        this.currentPrice = null;
+        this.nextBidPrice = null;
+    }
+
+    public BidException(String msg, Integer currentPrice, Integer nextBidPrice) {
+        super(msg);
+        this.currentPrice = currentPrice;
+        this.nextBidPrice = nextBidPrice;
     }
 }
