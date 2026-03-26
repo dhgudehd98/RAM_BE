10cd1344113f46fc0e82845226a4dcce04fb7cd1
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-03-26 15:29:25 +0900

    20260326_ignore : Test용 application-test.properties 추가

--- src/test/java/com/sh/Ram/auction/AuctinoCursorTest.java
+++ src/test/java/com/sh/Ram/auction/AuctinoCursorTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AuctinoCursorTest {
 
     @Autowired
