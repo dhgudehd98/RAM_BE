fe95bf96c535a370ec239e7bf4a5926a81243b24
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-03-26 15:17:14 +0900

    20260326_feat : 경매 리스트 Cursor를 활용한 무한 스크롤 테스트 코드 작성

--- src/test/java/com/sh/Ram/auction/AuctinoCursorTest.java
+++ src/test/java/com/sh/Ram/auction/AuctinoCursorTest.java
@@ -22,7 +22,7 @@ public class AuctinoCursorTest {
     @DisplayName("Cursor + QueryDSL을 활용한 전체 경매 조회  ")
     void findAllQueryDSL() {
         // 현재가 기준 내림 차순
-        List<AuctionDto> auctions = auctionService.auctionListQueryDSL(null, null, null);
+        List<AuctionDto> auctions = auctionService.auctionList(null, null, null);
 
         Assertions.assertThat(auctions.size()).isEqualTo(3);
     }
@@ -30,10 +30,10 @@ public class AuctinoCursorTest {
     @Test
     @DisplayName("Cursor + QueryDSL을 활용한 상태별 경매 조회 ")
     void findAuctionByStatus() {
-        List<AuctionDto> auctionsProgress = auctionService.auctionListQueryDSL(null, null, "progress");
+        List<AuctionDto> auctionsProgress = auctionService.auctionList(null, null, "progress");
         Assertions.assertThat(auctionsProgress.size()).isEqualTo(2);
 
-        List<AuctionDto> auctionsClosed = auctionService.auctionListQueryDSL(null, null, "closed");
+        List<AuctionDto> auctionsClosed = auctionService.auctionList(null, null, "closed");
         Assertions.assertThat(auctionsClosed.size()).isEqualTo(1);
     }
 
@@ -42,15 +42,15 @@ public class AuctinoCursorTest {
     @DisplayName("Cursor + QueryDSL을 경매 상품 정렬 ")
     void findAuctionOrderBy() {
         //given - orderBy CurrentPrice
-        List<AuctionDto> orderByCurrentPrice = auctionService.auctionListQueryDSL(null, "currentPrice", null);
+        List<AuctionDto> orderByCurrentPrice = auctionService.auctionList(null, "currentPrice", null);
         Assertions.assertThat(orderByCurrentPrice.get(0).getId()).isEqualTo(1035L);
 
         //given - order by StartDate
-        List<AuctionDto> orderByStartDate = auctionService.auctionListQueryDSL(null, "startDate", null);
+        List<AuctionDto> orderByStartDate = auctionService.auctionList(null, "startDate", null);
         Assertions.assertThat(orderByStartDate.get(1).getId()).isEqualTo(1036L);
 
         //given - order by endDate
-        List<AuctionDto> orderByEndDate = auctionService.auctionListQueryDSL(null, "endDate", null);
+        List<AuctionDto> orderByEndDate = auctionService.auctionList(null, "endDate", null);
         Assertions.assertThat(orderByEndDate.get(1).getId()).isEqualTo(1035L);
 
     }
