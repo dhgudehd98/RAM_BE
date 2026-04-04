a419a8f0e216973496e91610101e419e263e39b4
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-04-04 20:24:20 +0900

    20260404_feat : 상품 등록시 , DB에 존재하는 BrandList 출력

---
+++ src/main/java/com/sh/Ram/brand/dto/BrandDto.java
@@ -0,0 +1,16 @@
+package com.sh.Ram.brand.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BrandDto {
+
+    private Long brandId;
+    private String brandName;
+}
