0df27fe4ac9165a0f651c59105333671f0ba3fed
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-04-06 14:32:14 +0900

    20260406_update : properties jwt 수정

--- src/main/java/com/sh/Ram/elasticSearch/product/document/ProductDocument.java
+++ src/main/java/com/sh/Ram/elasticSearch/product/document/ProductDocument.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.Setting;
 
 import java.util.ArrayList;
@@ -32,6 +34,17 @@ public class ProductDocument {
     private String category;
     private List<String> tags = new ArrayList<>();
 
+    public ProductDocument(Long id, Long memberId, String name, String brand, Integer price, String imageUrl, String category, List<String> tags) {
+        this.id = id;
+        this.memberId = memberId;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.tags = tags;
+    }
 
-
+    @Field(type = FieldType.Dense_Vector, dims = 1536)
+    private float[] descriptionVector;
 }
