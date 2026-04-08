b21fd10b2fa3bc4f0503a4ca53a4932911cdb5c8
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-04-09 00:43:33 +0900

    20260408_feat : 스케줄러를 통해서 날씨 API 요청 및 해당 Vector Redis에 저장

--- src/main/java/com/sh/Ram/redis/StandAlonceConfig.java
+++ src/main/java/com/sh/Ram/redis/StandAlonceConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class StandAlonceConfig {
@@ -23,5 +27,18 @@ public class StandAlonceConfig {
     @Bean
     public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
         return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> weatherRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> weatherRedisTemplate = new RedisTemplate<>();
+        weatherRedisTemplate.setConnectionFactory(connectionFactory);
+
+        weatherRedisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        weatherRedisTemplate.setValueSerializer(serializer);
+
+        return weatherRedisTemplate;
     }
 }
---
+++ src/main/java/com/sh/Ram/weather/scheduler/WeatherScheduler.java
@@ -0,0 +1,36 @@
+package com.sh.Ram.weather.scheduler;
+
+
+import com.sh.Ram.redis.weather.RedisWeatherVector;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WeatherScheduler {
+
+
+    private final WeatherService weatherService;
+    private final EmbeddingModel embeddingModel;
+    private final RedisWeatherVector redisWeatherVector;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateWeatherVector() {
+
+        log.info("[날씨 + 문구 임베딩 작업 시작]");
+
+        String weatherInfo = weatherService.getWeatherInfo();
+        log.info("[Weather Info] : " + weatherInfo);
+
+        // 날씨에 대한 정보 + 추천 상품에 대한 문구 임베딩 모델을 통해 데이터 임베딩
+        float[] vectors = embeddingModel.embed(weatherInfo);
+
+        // Redis에 데이터 저장
+        redisWeatherVector.setWeatherVector(vectors);
+    }
+}
