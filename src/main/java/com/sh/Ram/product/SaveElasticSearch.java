1b1ea25460c56119826f97c3930035156170a043
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-04-11 01:43:54 +0900

    20260411_test : DEV ES Connection Test_v2

--- src/main/java/com/sh/Ram/product/SaveElasticSearch.java
+++ src/main/java/com/sh/Ram/product/SaveElasticSearch.java
@@ -1,53 +1,53 @@
-package com.sh.Ram.product;
-
-
-import com.sh.Ram.RAG.embedding.sevice.EmbeddingService;
-import com.sh.Ram.elasticSearch.product.document.ProductDocument;
-import com.sh.Ram.elasticSearch.product.repository.ProductDocumentRepository;
-import com.sh.Ram.entity.Product;
-import com.sh.Ram.product.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RequiredArgsConstructor
-@Component
-@Slf4j
-public class SaveElasticSearch  {
-    private final ProductDocumentRepository documentRepository;
-    private final ProductRepository repository;
-    private final EmbeddingModel embeddingModel;
-
-
-//    @Bean
-    @Transactional
-    public void run() {
-        List<Product> products = repository.findAll();
-        List<String> list = new ArrayList<>();
-        list.add("후드집업");
-        list.add("후드티");
-        list.add("상의");
-        int count = 1;
-        for (Product product : products) {
-
-            if(count == 301) break;
-
-            Long memberId = 4L;
-            ProductDocument document = new ProductDocument(
-                    product,
-                    list
-            );
-
-            document.setDescriptionVector(embeddingModel.embed(product.getDescription()));
-            documentRepository.save(document);
-
-            count ++;
-        }
-    }
-}
+//package com.sh.Ram.product;
+//
+//
+//import com.sh.Ram.RAG.embedding.sevice.EmbeddingService;
+//import com.sh.Ram.elasticSearch.product.document.ProductDocument;
+//import com.sh.Ram.elasticSearch.product.repository.ProductDocumentRepository;
+//import com.sh.Ram.entity.Product;
+//import com.sh.Ram.product.repository.ProductRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.ai.embedding.EmbeddingModel;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Component
+//@Slf4j
+//public class SaveElasticSearch  {
+//    private final ProductDocumentRepository documentRepository;
+//    private final ProductRepository repository;
+//    private final EmbeddingModel embeddingModel;
+//
+//
+////    @Bean
+//    @Transactional
+//    public void run() {
+//        List<Product> products = repository.findAll();
+//        List<String> list = new ArrayList<>();
+//        list.add("후드집업");
+//        list.add("후드티");
+//        list.add("상의");
+//        int count = 1;
+//        for (Product product : products) {
+//
+//            if(count == 301) break;
+//
+//            Long memberId = 4L;
+//            ProductDocument document = new ProductDocument(
+//                    product,
+//                    list
+//            );
+//
+//            document.setDescriptionVector(embeddingModel.embed(product.getDescription()));
+//            documentRepository.save(document);
+//
+//            count ++;
+//        }
+//    }
+//}
