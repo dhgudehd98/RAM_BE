36479abe2765f36461b05854c36b08f576b597d2
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-04-20 16:15:20 +0900

    20260420_exception : AuctionAgentException ExceptionHandler 추가

---
+++ src/main/java/com/sh/Ram/common/exception/auctionAgent/AuctionAgentException.java
@@ -0,0 +1,8 @@
+package com.sh.Ram.common.exception.auctionAgent;
+
+public class AuctionAgentException extends RuntimeException{
+
+    public AuctionAgentException(String message) {
+        super(message);
+    }
+}
